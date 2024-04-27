fbb5a41e5934eba66b9a5406e2b9a67788a52e39
Hakkar <zugrutus@gmail.com>
2024-04-28 00:43:48 +0100

    postgres26

--- src/controllers/picturesController.ts
+++ src/controllers/picturesController.ts
@@ -7,65 +7,124 @@ interface MulterRequest extends Request {
   file: any
 }
 
-const pictureController = {
-  getPictures: async (req: Request, res: Response) => {
-    const { page = 1, page_size = 4 }: { page?: number; page_size?: number } =
-      req.query;
-    const offset = (page - 1) * page_size;
+export const getPictures = async (req: Request, res: Response) => {
+  const { page = 1, page_size = 4 }: { page?: number; page_size?: number } =
+    req.query;
+  const offset = (page - 1) * page_size;
 
-    try {
-      const query = `SELECT * FROM pictures ORDER BY id LIMIT $1 OFFSET $2`;
-      const values = [page_size, offset];
-      const { rows } = await pool.query(query, values);
+  try {
+    const query = `SELECT * FROM pictures ORDER BY id LIMIT $1 OFFSET $2`;
+    const values = [page_size, offset];
+    const { rows } = await pool.query(query, values);
 
-      if (rows.length === 0) {
-        return res
-          .status(404)
-          .json({ error: "No pictures found for the given page" });
-      }
-
-      const pictures = rows.map(({ id, cloudinary_id, cloudinary_url }) => ({
-        id: id as number,
-        cloudinary_id: cloudinary_id as string,
-        cloudinary_url: cloudinary_url as string,
-      }));
-      res.json(pictures);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Internal server error" });
+    if (rows.length === 0) {
+      return res
+        .status(404)
+        .json({ error: "No pictures found for the given page" });
     }
-  },
-  postPicture: async (req: Request, res: Response) => {
-    try {
-      const image = (req as MulterRequest).file;
-      if (!image) {
-        throw new AppError(404, "Image file not found");
-      }
 
-      const imagePath = image.path;
-      const pathArr = imagePath.split("/");
-      const length = pathArr.length;
+    const pictures = rows.map(({ id, cloudinary_id, cloudinary_url }) => ({
+      id: id as number,
+      cloudinary_id: cloudinary_id as string,
+      cloudinary_url: cloudinary_url as string,
+    }));
+    res.json(pictures);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
-      // Upload the image to Cloudinary
-      const result = await cloudinary.uploader.upload(pathArr[length - 1]);
-
-      // Insert the picture into the database
-      const query = {
-        name: "insert-picture",
-        text: "INSERT INTO pictures (cloudinary_url, cloudinary_id) VALUES ($1, $2)",
-        values: [result.url, result.public_id],
-      };
-
-      await pool.query(query);
-
-      // Send a JSON response with the successful upload information
-      res.json({
-        message: "Picture uploaded successfully",
-      });
-    } catch (error) {
-      console.log(error);
+export const addPicture = async (req: Request, res: Response) => {
+  try {
+    const image = (req as MulterRequest).file;
+    if (!image) {
+      throw new AppError(404, "Image file not found");
     }
-  },
+
+    const imagePath = image.path;
+    const pathArr = imagePath.split("/");
+    const length = pathArr.length;
+
+    // Upload the image to Cloudinary
+    const result = await cloudinary.uploader.upload(pathArr[length - 1]);
+
+    // Insert the picture into the database
+    const query = {
+      name: "insert-picture",
+      text: "INSERT INTO pictures (cloudinary_url, cloudinary_id) VALUES ($1, $2)",
+      values: [result.url, result.public_id],
+    };
+
+    await pool.query(query);
+
+    // Send a JSON response with the successful upload information
+    res.json({
+      message: "Picture uploaded successfully",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+// const pictureController = {
+//   getPictures: async (req: Request, res: Response) => {
+//     const { page = 1, page_size = 4 }: { page?: number; page_size?: number } =
+//       req.query;
+//     const offset = (page - 1) * page_size;
+
+//     try {
+//       const query = `SELECT * FROM pictures ORDER BY id LIMIT $1 OFFSET $2`;
+//       const values = [page_size, offset];
+//       const { rows } = await pool.query(query, values);
+
+//       if (rows.length === 0) {
+//         return res
+//           .status(404)
+//           .json({ error: "No pictures found for the given page" });
+//       }
+
+//       const pictures = rows.map(({ id, cloudinary_id, cloudinary_url }) => ({
+//         id: id as number,
+//         cloudinary_id: cloudinary_id as string,
+//         cloudinary_url: cloudinary_url as string,
+//       }));
+//       res.json(pictures);
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: "Internal server error" });
+//     }
+//   },
+//   postPicture: async (req: Request, res: Response) => {
+//     try {
+//       const image = (req as MulterRequest).file;
+//       if (!image) {
+//         throw new AppError(404, "Image file not found");
+//       }
+
+//       const imagePath = image.path;
+//       const pathArr = imagePath.split("/");
+//       const length = pathArr.length;
+
+//       // Upload the image to Cloudinary
+//       const result = await cloudinary.uploader.upload(pathArr[length - 1]);
+
+//       // Insert the picture into the database
+//       const query = {
+//         name: "insert-picture",
+//         text: "INSERT INTO pictures (cloudinary_url, cloudinary_id) VALUES ($1, $2)",
+//         values: [result.url, result.public_id],
+//       };
+
+//       await pool.query(query);
+
+//       // Send a JSON response with the successful upload information
+//       res.json({
+//         message: "Picture uploaded successfully",
+//       });
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   },
 //   deletePicture: async (req: Request, res: Response) => {
 //   try {
 //     const pictureId = parseInt(req.params.id);
@@ -105,6 +164,6 @@ const pictureController = {
 //     }
 //   }
 // }
-};
+// };
 
-export default pictureController;
+// export default pictureController;
--- src/routes/pictureRoute.ts
+++ src/routes/pictureRoute.ts
@@ -1,11 +1,11 @@
 import express, { Router } from "express";
-import pictureController from "../controllers/picturesController";
+import {getPictures, addPicture} from "../controllers/picturesController";
 import { upload } from '../config/cloudinaryConfig';
 
 const router = Router();
 
-router.get("/", pictureController.getPictures);
-router.post("/add", upload.single("image"), pictureController.postPicture);
+router.get("/", getPictures);
+router.post("/add", upload.single("image"), addPicture);
 // router.post("/:id/delete", pictureController.deletePicture);
 
 export default router;
