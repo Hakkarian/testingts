5aeae49e23f0a48f5b09ef67296785f320ca3646
Hakkar <zugrutus@gmail.com>
2024-04-28 00:40:14 +0100

    postgres25

--- src/config/cloudinaryConfig.ts
+++ src/config/cloudinaryConfig.ts
@@ -5,8 +5,7 @@ import multer from "multer";
 const cloudinaryConfig = {
   cloud_name: process.env.CLOUD_NAME as string,
   api_key: process.env.CLOUD_API_KEY as string,
-  api_secret: process.env.CLOUD_API_SECRET as string,
-  secure: true,
+  api_secret: process.env.CLOUD_API_SECRET as string
 };
 cloudinary.config(cloudinaryConfig);
 
--- src/routes/pictureRoute.ts
+++ src/routes/pictureRoute.ts
@@ -1,11 +1,11 @@
-import express, { Router, Request, Response } from "express";
+import express, { Router } from "express";
 import pictureController from "../controllers/picturesController";
 import { upload } from '../config/cloudinaryConfig';
 
 const router = Router();
 
 router.get("/", pictureController.getPictures);
-// router.post("/add", (req: Request, res: Response) => res.json("This is fine"));
+router.post("/add", upload.single("image"), pictureController.postPicture);
 // router.post("/:id/delete", pictureController.deletePicture);
 
 export default router;
