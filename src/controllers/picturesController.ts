7d4a0905298b199686292a72fb471e59f4d13287
Hakkar <zugrutus@gmail.com>
2024-04-27 20:37:35 +0100

    postgres2

--- src/controllers/picturesController.ts
+++ src/controllers/picturesController.ts
@@ -14,22 +14,22 @@ const pictureController = {
     const offset = (page - 1) * page_size;
 
     try {
-      const query = `SELECT * FROM pictures ORDER BY id LIMIT $1 OFFSET $2`;
-      const values = [page_size, offset];
-      const { rows } = await pool.query(query, values);
+      // const query = `SELECT * FROM pictures ORDER BY id LIMIT $1 OFFSET $2`;
+      // const values = [page_size, offset];
+      // const { rows } = await pool.query(query, values);
 
-      if (rows.length === 0) {
-        return res
-          .status(404)
-          .json({ error: "No pictures found for the given page" });
-      }
+      // if (rows.length === 0) {
+      //   return res
+      //     .status(404)
+      //     .json({ error: "No pictures found for the given page" });
+      // }
 
-      const pictures = rows.map(({ id, cloudinary_id, cloudinary_url }) => ({
-        id: id as number,
-        cloudinary_id: cloudinary_id as string,
-        cloudinary_url: cloudinary_url as string,
-      }));
-      res.json(pictures);
+      // const pictures = rows.map(({ id, cloudinary_id, cloudinary_url }) => ({
+      //   id: id as number,
+      //   cloudinary_id: cloudinary_id as string,
+      //   cloudinary_url: cloudinary_url as string,
+      // }));
+      res.json("fine");
     } catch (error) {
       console.log("there is an error", error)
       res.status(500).json({ error: "Internal server error" });
