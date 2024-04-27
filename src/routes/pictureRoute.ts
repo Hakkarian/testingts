1801625d80a58ad901e7e2bc50e77838a8724200
Hakkar <zugrutus@gmail.com>
2024-04-28 00:56:03 +0100

    code modularity

--- src/routes/pictureRoute.ts
+++ src/routes/pictureRoute.ts
@@ -1,11 +1,13 @@
 import express, { Router } from "express";
-import {getPictures, addPicture} from "../controllers/picturesController";
+import { getPictures } from "../controllers/pictures/get";
 import { upload } from '../config/cloudinaryConfig';
+import addPicture from "../../src/controllers/pictures/add";
+import deletePicture from "../../src/controllers/pictures/delete";
 
 const router = Router();
 
 router.get("/", getPictures);
 router.post("/add", upload.single("image"), addPicture);
-// router.post("/:id/delete", pictureController.deletePicture);
+router.post("/:id/delete", deletePicture);
 
 export default router;
