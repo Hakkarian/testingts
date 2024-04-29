76265241cf56b5f7f0fec2d5c98eaa6172f46704
Hakkar <zugrutus@gmail.com>
2024-04-29 03:08:50 +0100

    fixing post route2

--- src/controllers/pictures/add.ts
+++ src/controllers/pictures/add.ts
@@ -11,8 +11,6 @@ interface MulterRequest extends Request {
 const addPicture = async (req: Request, res: Response) => {
   try {
     const image = (req as MulterRequest).file;
-    const pictures = [];
-    console.log(image)
     if (!image) {
       throw new AppError(404, "Image file not found");
     }
@@ -23,19 +21,25 @@ const addPicture = async (req: Request, res: Response) => {
 
     await sql`INSERT INTO pictures (cloudinary_url, cloudinary_id) VALUES (${result.url}, ${result.public_id})`;
 
-    const picture = await sql`SELECT * FROM pictures WHERE cloudinary_url = ${result.url} AND cloudinary_id = ${result.public_id}`;
-    const totalCount = await sql`SELECT COUNT(*) FROM pictures`;
+    const { rows } = await sql`
+      SELECT * FROM pictures
+      WHERE cloudinary_url = ${result.url}
+        AND cloudinary_id = ${result.public_id}
+      LIMIT 1
+    `;
 
-    const images = pictures.push(picture);
+    const picture = rows[0];
+    const totalCount = (await sql`SELECT COUNT(*) FROM pictures`).rows[0].count;
 
     // Send a JSON response with the successful upload information
     res.json({
       message: "Picture uploaded successfully",
-      images, totalCount
+      picture, totalCount
     });
   } catch (error) {
     console.log(error);
   }
 };
+
 
 export default addPicture;
