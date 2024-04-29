816edbef5a672c8a678bbbec97efddf6da5c15af
Hakkar <zugrutus@gmail.com>
2024-04-29 02:52:49 +0100

    fixing post route

--- src/controllers/pictures/add.ts
+++ src/controllers/pictures/add.ts
@@ -11,6 +11,7 @@ interface MulterRequest extends Request {
 const addPicture = async (req: Request, res: Response) => {
   try {
     const image = (req as MulterRequest).file;
+    const pictures = [];
     console.log(image)
     if (!image) {
       throw new AppError(404, "Image file not found");
@@ -22,9 +23,15 @@ const addPicture = async (req: Request, res: Response) => {
 
     await sql`INSERT INTO pictures (cloudinary_url, cloudinary_id) VALUES (${result.url}, ${result.public_id})`;
 
+    const picture = await sql`SELECT * FROM pictures WHERE cloudinary_url = ${result.url} AND cloudinary_id = ${result.public_id}`;
+    const totalCount = await sql`SELECT COUNT(*) FROM pictures`;
+
+    const images = pictures.push(picture);
+
     // Send a JSON response with the successful upload information
     res.json({
       message: "Picture uploaded successfully",
+      images, totalCount
     });
   } catch (error) {
     console.log(error);
