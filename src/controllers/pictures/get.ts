46fd422ed3b70ea6b8b3f7ecddb11bc5ad41ce43
Hakkar <zugrutus@gmail.com>
2024-05-19 14:25:56 +0100

    implementing reusable route3

--- src/controllers/pictures/get.ts
+++ src/controllers/pictures/get.ts
@@ -35,48 +35,7 @@ const getPictures = async (req: Request, res: Response) => {
     const totalCount = (await sql`SELECT COUNT(*) FROM pictures`).rows[0].count;
 
     res.json({
-      pictures, totalCount, keyword
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-};
-
-const getRiseyPictures = async (req: Request, res: Response) => {
-  const page = parseInt(req.query.page as string) || 1;
-  const perPage = parseInt(req.query.per_page as string) || 4;
-  const offset = (page - 1) * perPage;
-
-  try {
-    // Query to get the subset of pictures with LIMIT and OFFSET
-    const { rows } = await sql`
-      SELECT * FROM pictures
-      ORDER BY id
-      LIMIT ${perPage} OFFSET ${offset}
-    `;
-
-    console.log("🚀 ~ getPictures ~ rows:", rows);
-
-    if (rows.length === 0) {
-      return res
-        .status(404)
-        .json({ error: "No pictures found for the given page" });
-    }
-
-    const pictures = rows.map(({ id, cloudinary_id, cloudinary_url }) => ({
-      id: id as number,
-      cloudinary_id: cloudinary_id as string,
-      cloudinary_url: cloudinary_url as string,
-    }));
-
-    console.log("🚀 ~ pictures ~ pictures:", pictures);
-
-    const totalCount = (await sql`SELECT COUNT(*) FROM pictures`).rows[0].count;
-
-    res.json({
-      pictures,
-      totalCount,
+      pictures, totalCount, code: keyword
     });
   } catch (error) {
     console.error(error);
